ea25f54372100cf6520f0c69812a31012c1eb1ed
Mikat <jsmikat@outlook.com>
2024-07-04 03:44:28 +0600

    Add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import Hero from "@/components/Hero";
 
 const page = () => {
   return (
-    <div className="w-full h-screen p-8 flex flex-col gap-4 items-center justify-center">
-      <h1 className="heading-1">Welcome to FlowCount</h1>
-      <p className="paragraph mx-12 text-center">
-        FlowCount is a simple expense tracker that helps you keep track of your
-        expenses.
-      </p>
-      <Link href="/dashboard">
-        <Button>Get Started</Button>
-      </Link>
+    <div className="w-screen h-screen flex flex-col gap-4 items-center justify-center">
+      <Hero />
     </div>
   );
 };
---
+++ components/Hero.tsx
@@ -0,0 +1,41 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+function Hero() {
+  return (
+    <>
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+          <h1 className="font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+            Manage Your Expenses Effortlessly with Our Next.js 14 Powered App
+          </h1>
+          <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            Take control of your finances with our open-source expense tracker.
+            Follow our journey as we develop and refine this powerful tool.
+          </p>
+          <div className="space-x-4">
+            <Link
+              href="/dashboard"
+              className={cn(
+                buttonVariants({ size: "lg" }),
+                "bg-primary-400 hover:bg-primary-200"
+              )}
+            >
+              Get Started
+            </Link>
+            <Link
+              href={"#"}
+              target="_blank"
+              className={cn(buttonVariants({ variant: "outline", size: "lg" }), "outline-primary-400")}
+            >
+              View on GitHub
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Hero;
